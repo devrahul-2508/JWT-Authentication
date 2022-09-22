16937cb242aaa51a0d08a910fc8aefe287da7622
Rahul Roy <devrahul2508@gmail.com>
2022-09-22 11:11:35 +0530

    Commits

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require("express");
 const jwt = require("jsonwebtoken")
 
@@ -17,15 +18,34 @@ const posts= [
 ]
 
 
-app.get("/posts",(req,res)=>{
-    res.json(posts);
+app.get("/posts",authenticateToken,(req,res)=>{
+    res.json(posts.filter(post => post.username === req.user.name));
 })
 
 app.post("/login",(req,res)=>{
     //Authenticate User
 
     const username = req.body.username
+    const user = {name: username}
+
+    const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
+    res.json({
+        accessToken: accessToken
+
+    })
 })
+
+function authenticateToken(req,res,next){
+    const authHeader = req.headers["authorization"]
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) return res.status(401)
+
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+        if(err) return res.status(403);
+        req.user = user
+        next()
+    })
+}
 
 
 app.listen(3000,()=>{
